3496d40f5679697a792e680ccbf40b675de30a7b
Billy Dotson <billybad5@gmail.com>
2022-09-14 17:23:00 -0400

    Add project files.

---
+++ WorkingWithNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace WorkingWithNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // + add, - subtract, * times, / Divide, % Modulo
+            int num = 6;
+            num++; // add 1 to num
+            num--; // subtracts 1 from num
+            Console.WriteLine(num);
+            Console.ReadLine();
+            // Math. calls different math methods
+            Console.WriteLine(Math.Abs(-40));//absolute value
+            Console.WriteLine(Math.Sqrt(36));// square root
+            Console.WriteLine(Math.Max(4, 90));// Highest number
+            Console.WriteLine(Math.Round(4.6));// rounds the number
+            Console.ReadLine();
+        }
+    }
+}
